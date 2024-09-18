e412c69578238c9805f7e6b618df7140461648c2
ConCaerus <connorkeeterheise@gmail.com>
2024-09-18 08:45:19 -0500

    lighting + mine box collider increase + trash can tutorial box

---
+++ DesolateHaze/Assets/Scripts/DestroyerInstance.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class DestroyerInstance : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision col) {
+        if(col.gameObject.tag == "Player")
+            TransitionCanvas.I.loadGameAfterDeath(0f);
+        else Destroy(col.gameObject);
+    }
+}
