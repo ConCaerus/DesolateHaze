6cbd856045c0b58b93469d5c19c2b6d4779052a7
ConCaerus <connorkeeterheise@gmail.com>
2024-09-23 08:42:58 -0500

    added player view range

--- DesolateHaze/Assets/Scripts/Player/ViewRangeCol.cs
+++ DesolateHaze/Assets/Scripts/Player/ViewRangeCol.cs
@@ -1,15 +1,28 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class ViewRangeCol : MonoBehaviour {
+    [SerializeField] float viewRange;
+    List<MeshRenderer> envRenderers = new List<MeshRenderer>();
 
     private void Awake() {
-        
+        foreach(var i in GameObject.FindGameObjectsWithTag("Environment")) {
+            if(i.TryGetComponent<MeshRenderer>(out var m)) {
+                m.enabled = false;
+                envRenderers.Add(m);
+            }
+        }
     }
 
-    private void OnTriggerEnter(Collider col) {
-        
+    private void LateUpdate() {
+        manageEnvironment();
     }
-    private void OnTriggerExit(Collider col) {
-        
+
+    void manageEnvironment() {
+        foreach(var i in envRenderers) {
+            bool close = Mathf.Abs(i.transform.position.x - transform.position.x) < viewRange;
+            if(close && !i.enabled) i.enabled = true;
+            else if(!close && i.enabled) i.enabled = false;
+        }
     }
 }
