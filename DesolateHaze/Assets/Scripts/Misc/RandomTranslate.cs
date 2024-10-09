61f7a6e1471536b315479705489e454352a20137
MasterBlade27 <hal.lam@trojans.dsu.edu>
2024-10-09 16:50:31 -0500

    More Busted Bridge + RandTranslate Script

---
+++ DesolateHaze/Assets/Scripts/Misc/RandomTranslate.cs
@@ -0,0 +1,158 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+[ExecuteInEditMode]
+
+public class RandomTranslate : MonoBehaviour
+{
+    private int timer;
+    [SerializeField]
+    private List<Transform> Groupign;
+    [SerializeField]
+    private bool Parent;
+    [SerializeField]
+    private bool LockPosX, LockPosY, LockPosZ;
+    [SerializeField]
+    private bool LockRotX, LockRotY, LockRotZ;
+    [SerializeField]
+    private int MinRange, MaxRange;
+
+    private float Min, Max;
+
+    public void Starto()
+    {
+        Min = (float)MinRange;
+        Max = (float)MaxRange;
+
+        timer = Random.Range(4, 20);
+
+        if (Parent)
+            PRando();
+        else
+            RANDO();
+    }
+
+    void PRando()
+    {
+        foreach (Transform t in Groupign)
+        {
+            Vector3 pos = new Vector3();
+
+            for (int i = 0; i < timer; i++)
+            {
+                pos = Vector3.zero;
+
+                if (!LockPosX)
+                    pos.x = Random.Range(Min, Max);
+                if (!LockPosY)
+                    pos.y = Random.Range(Min, Max);
+                if (!LockPosZ)
+                    pos.z = Random.Range(Min, Max);
+
+                Undo.RecordObject(t,"Undo Translation");
+                t.position += pos;
+            }
+        }
+    }
+
+    void RANDO()
+    {
+        Vector3 pos = new Vector3();
+
+        for (int i = 0; i < timer; i++)
+        {
+            pos = Vector3.zero;
+
+            if(!LockPosX)
+                pos.x = Random.Range(Min, Max);
+            if (!LockPosY)
+                pos.y = Random.Range(Min, Max);
+            if (!LockPosZ)
+                pos.z = Random.Range(Min, Max);
+
+            Undo.RecordObject(transform, "Undo Translation");
+            transform.position += pos;
+        }
+    }
+
+    public void Rotato()
+    {
+        Min = (float)MinRange;
+        Max = (float)MaxRange;
+
+        timer = Random.Range(4, 20);
+
+        if (Parent)
+            PRoto();
+        else
+            ROTO();
+    }
+
+    void PRoto()
+    {
+        foreach (Transform t in Groupign)
+        {
+            Vector3 Rototo = new Vector3();
+
+            for (int i = 0; i < timer; i++)
+            {
+                Rototo = Vector3.zero;
+
+                if (!LockRotX)
+                    Rototo.x = Random.Range(Min, Max);
+                if (!LockRotY)
+                    Rototo.y = Random.Range(Min, Max);
+                if (!LockRotZ)
+                    Rototo.z = Random.Range(Min, Max);
+
+                Quaternion QU;
+                QU = Quaternion.Euler(Rototo);
+
+                Undo.RecordObject(t, "Undo Rotation");
+                t.rotation = QU;
+            }
+        }
+    }
+
+    void ROTO()
+    {
+        Vector3 Rototo = new Vector3();
+
+        for (int i = 0; i < timer; i++)
+        {
+            Rototo = Vector3.zero;
+
+            if(!LockRotX)
+                Rototo.x = Random.Range(Min, Max);
+            if (!LockRotY)
+                Rototo.y = Random.Range(Min, Max);
+            if (!LockRotZ)
+                Rototo.z = Random.Range(Min, Max);
+            
+            Quaternion QU;
+            QU = Quaternion.Euler(Rototo);
+
+            Undo.RecordObject(transform, "Undo Rotation");
+            transform.rotation = QU;
+        }
+    }
+}
+
+[CustomEditor(typeof(RandomTranslate))]
+public class RandTransCI : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        RandomTranslate Button = (RandomTranslate)target;
+
+        if (GUILayout.Button("Random"))
+            Button.Starto();
+
+        if (GUILayout.Button("Rotate"))
+            Button.Rotato();
+
+    }
+}
