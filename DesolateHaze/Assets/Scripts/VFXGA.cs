09b187619e938a02000663a39b8d7c5c53f43e24
MasterBlade27 <hal.lam@trojans.dsu.edu>
2024-10-23 16:19:54 -0500

    Added Blinking Landmines + Added the explosion VFX
    HAVE YET TO IMPLEMENT VFX, the code doesnt work

---
+++ DesolateHaze/Assets/Scripts/VFXGA.cs
@@ -0,0 +1,14 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFXGA : GameAction
+{
+    [SerializeField]
+    private VisualEffect vfx;
+
+    public override void Action()
+    {
+        vfx.Play();
+    }
+}*/
