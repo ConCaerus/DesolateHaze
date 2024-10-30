284b9edca27b0acbb459055834aee281d3157cc7
MasterBlade27 <hal.lam@trojans.dsu.edu>
2024-10-30 16:53:28 -0500

    CROW FLAPPING
    Implemented the first crow flying off, adding the rest soon

--- DesolateHaze/Assets/Scripts/Monsters/CrowInstance.cs
+++ DesolateHaze/Assets/Scripts/Monsters/CrowInstance.cs
@@ -5,11 +5,27 @@ public class CrowInstance : MonoBehaviour {
     [SerializeField] Rigidbody rb;
 
     [SerializeField] AudioPoolInfo caw;
+    [SerializeField] Animator CrowAnim;
+    [SerializeField] float time;
+    [SerializeField] bool BOn = false;
 
     public void flyOff() {
+        time = 0f;
+        BOn = true;
+
         rb.linearVelocity = exitVel;
         Destroy(gameObject, 1f);
 
         AudioManager.I.playSound(caw, transform.position, 1f);
+    }
+
+    public void Update()
+    {
+        if (BOn == true)
+        {
+            Debug.Log("SCAW");
+            time += Time.deltaTime;
+            CrowAnim.SetFloat("Blend", time);
+        }
     }
 }
