b84a70c5391f5a57d5366a2d7c6d9d6bd665cbc3
ConCaerus <connorkeeterheise@gmail.com>
2025-02-12 08:41:36 -0600

    more meat mound things

--- DesolateHaze/Assets/Scripts/Monsters/MeatMoundInstance.cs
+++ DesolateHaze/Assets/Scripts/Monsters/MeatMoundInstance.cs
@@ -1,33 +1,44 @@
 using System.Collections;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class MeatMoundInstance : MonoBehaviour {
     [SerializeField] Transform pivot, tip;
     [SerializeField] Animator anim;
     [SerializeField] float strikeDist;
+    [SerializeField] float rotSpeed;
+    [SerializeField] float animSpeedMod;
 
     Transform playerTrans;
+
+    Coroutine halter = null;
 
     bool hasKilledPlayer = false;
 
     private void OnTriggerStay(Collider other) {
         if(hasKilledPlayer) return;
-        movePivot();
+        if(halter == null)
+            movePivot();
         strikeCheck();
     }
 
     private void Start() {
         playerTrans = PlayerMovement.I.transform;
+        anim.speed = animSpeedMod;
     }
 
     void movePivot() {
         var offset = playerTrans.position - pivot.position;
-        pivot.localEulerAngles = Vector3.forward * Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        var target = Vector3.forward * Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        if(target.z < 0f) target.z += 360f;
+        pivot.localEulerAngles = Vector3.Lerp(pivot.localEulerAngles, target, rotSpeed * 100f * Time.deltaTime);
     }
 
     void strikeCheck() {
-        if(Vector3.Distance(pivot.position, playerTrans.position) < strikeDist)
+        if(halter == null && Vector3.Distance(pivot.position, playerTrans.position) < strikeDist) {
             anim.SetTrigger("strike");
+            halter = StartCoroutine(halt());
+        }
     }
 
     public void killPlayer() {
@@ -44,5 +55,10 @@ public class MeatMoundInstance : MonoBehaviour {
             playerTrans.position = tip.position + hitOffset;
             yield return new WaitForEndOfFrame();
         }
+    }
+
+    IEnumerator halt() {
+        yield return new WaitForSeconds(.5f * (1f / animSpeedMod));
+        halter = null;
     }
 }
