5a436ceadb245a63f3d9006236fb1cf8f3bad711
ConCaerus <connorkeeterheise@gmail.com>
2025-04-09 08:33:47 -0500

    main menu changes

--- DesolateHaze/Assets/Scripts/UI/MainMenuCanvas.cs
+++ DesolateHaze/Assets/Scripts/UI/MainMenuCanvas.cs
@@ -1,13 +1,20 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MainMenuCanvas : MonoBehaviour {
     [SerializeField] Button selectOnStart;
+    [SerializeField] List<GameObject> backgrounds = new List<GameObject>();
 
     private void Start() {
         Time.timeScale = 1f;
         selectOnStart.Select();
         ControlSchemeManager.runOnChange += (keyb) => { selectOnStart.Select(); };
+
+        var ind = (int)Saver.getCurArea();
+        Debug.Log(Saver.getCurArea().ToString() + " " + ind);
+        for(int i = 0; i < backgrounds.Count; i++) 
+            backgrounds[i].SetActive(i == ind);
     }
 
     //  buttons
