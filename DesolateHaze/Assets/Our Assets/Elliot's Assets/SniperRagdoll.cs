733938965c34d160cf041f54d59e906746c48777
FreezShocka <elliot.plagge@trojans.dsu.edu>
2025-04-10 14:43:33 -0500

    implementing sniper rig

---
+++ DesolateHaze/Assets/Our Assets/Elliot's Assets/SniperRagdoll.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class SniperRagdoll : MonoBehaviour
+{
+    public GameObject sniperRig;
+    Collider[] ragdollColliders;
+    Rigidbody[] limbsRigidbodies;
+
+    private void Start()
+    {
+        RagdollMode(false);
+    }
+
+    public void RagdollMode(bool state)
+    {
+        ragdollColliders = sniperRig.GetComponentsInChildren<Collider>();
+        limbsRigidbodies = sniperRig.GetComponentsInChildren<Rigidbody>();
+
+        foreach (Collider col in ragdollColliders)
+        {
+            col.enabled = state;
+        }
+        foreach (Rigidbody rigid in limbsRigidbodies)
+        {
+            rigid.isKinematic = !state;
+        }
+    }
+}
