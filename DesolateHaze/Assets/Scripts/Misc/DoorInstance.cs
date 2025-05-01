ee45d5b30bdfbbb01c83d0710c288fc25f972eed
ConCaerus <connorkeeterheise@gmail.com>
2025-05-01 13:29:47 -0500

    added water tank model + replaced placeholder door

--- DesolateHaze/Assets/Scripts/Misc/DoorInstance.cs
+++ DesolateHaze/Assets/Scripts/Misc/DoorInstance.cs
@@ -3,19 +3,22 @@ using UnityEngine;
 
 public class DoorInstance : MonoBehaviour {
     [SerializeField] Vector3 endOffset;
+    [SerializeField] bool localSpace = false;
     Vector3 startPos;
 
-    private void Start() {
-        startPos = transform.position;
+    private void Awake() {
+        startPos = localSpace ? transform.localPosition : transform.position;
     }
 
     public void moveToEnd(float dur) {
         transform.DOKill();
-        transform.DOMove(startPos + endOffset, dur);
+        if(localSpace) transform.DOLocalMove(startPos + endOffset, dur);
+        else transform.DOMove(startPos + endOffset, dur);
     }
     public void moveToStart(float dur) {
         transform.DOKill();
-        transform.DOMove(startPos, dur);
+        if(localSpace) transform.DOLocalMove(startPos, dur);
+        else transform.DOMove(startPos, dur);
     }
 
     public Vector3 getOffset() {
